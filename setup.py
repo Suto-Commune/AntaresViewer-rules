ae8d4c34b56d4b099b861fe155fa61dc2c8ca0f3
Roren <RorenNatsumi@outlook.com>
2024-02-16 01:00:49 +0800

    创建目录结构

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='AntaresViewer-rules',
+    version='0.1',
+    description='一个包为了Antare的规则处理',
+    author='Roren Natsumi',
+    author_email='RorenNatsumi@outlook.com',
+    packages=['AntaresViewer-rules'],
+    install_requires='requirements.txt',
+)
